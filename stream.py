6693488d8b2f9ea6cb3d31c07bf4e3c7a79ba35c
Abhishekdutt9 <54331760+Abhishekdutt9@users.noreply.github.com>
2022-11-20 17:35:22 -0500

    Update stream.py

--- stream.py
+++ stream.py
@@ -34,5 +34,8 @@ m_jjj = st.number_input('m_jjj', min_value=0.001, max_value=10.0, value=1.0,key=
 if st.button('Predict '):
 
     label = predict(m_bb,m_wwbb,jet_1_pt,m_wbb,m_jjj)
-    st.success(f'The predicte value is {label[0]:.2f}')
+    if label[0]>-1:
+        st.success(f'The predicte value is: High Energy Particle')
+    else:
+        st.success(f'The predicte value is: Not a High Energy Particle')
 
