69176c0209a91b8be591e1a7068cc7059addaeaa
Abhishekdutt9 <54331760+Abhishekdutt9@users.noreply.github.com>
2022-11-19 20:30:49 -0500

    Update stream.py

--- stream.py
+++ stream.py
@@ -16,15 +16,15 @@ def predict(m_bb,m_wwbb,jet_1_pt,m_wbb,m_jjj):
 st.title('HIGGS BOSON Predictor')
 st.header('Enter the characteristics of the particle:')
 
-m_bb = st.number_input('m_bb:', min_value=0.001, max_value=10.0, value=1.0,key="1")
+m_bb = st.number_input('m_bb:', min_value=0.001, max_value=10.0, value=1.0,key="1",format="%.8f")
 
-m_wwbb = st.number_input('m_wwbb:', min_value=0.001, max_value=10.0, value=1.0,key="2")
+m_wwbb = st.number_input('m_wwbb:', min_value=0.001, max_value=10.0, value=1.0,key="2",format="%.8f")
 
-jet_1_pt = st.number_input('jet_1_pt', min_value=0.001, max_value=10.0, value=1.0,key="3")
+jet_1_pt = st.number_input('jet_1_pt', min_value=0.001, max_value=10.0, value=1.0,key="3",format="%.8f")
 
-m_wbb = st.number_input('m_wbb', min_value=0.001, max_value=10.0, value=1.0,key="4")
+m_wbb = st.number_input('m_wbb', min_value=0.001, max_value=10.0, value=1.0,key="4",format="%.8f")
 
-m_jjj = st.number_input('m_jjj', min_value=0.001, max_value=10.0, value=1.0,key="5")
+m_jjj = st.number_input('m_jjj', min_value=0.001, max_value=10.0, value=1.0,key="5",format="%.8f")
 
 
 if st.button('Predict '):
