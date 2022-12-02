e654c2e7728b4ce7f7ccc60ff5fc0db957b8c3c0
Abhishekdutt9 <54331760+Abhishekdutt9@users.noreply.github.com>
2022-12-01 21:15:40 -0500

    Update stream.py

--- stream.py
+++ stream.py
@@ -21,15 +21,15 @@ st.title('HIGGS BOSON Predictor')
 st.header('Enter the characteristics of the particle:')
 st.text("The Higgs boson is the fundamental particle associated with the Higgs field, a field that gives mass to other fundamental particles such as electrons and quarks. A particle's mass determines how much it resists changing its speed or position when it encounters a force. Not all fundamental particles have mass.")
 
-m_bb = st.number_input('m_bb:', min_value=0.001, max_value=10.0, value=1.0,key="1",format="%.4f")
+m_bb = st.number_input('m_bb:The estimated mass mH of the Higgs boson candidate, obtained through a probabilistic phase space integration.', min_value=0.001, max_value=10.0, value=1.0,key="1",format="%.4f")
 
-m_wwbb = st.number_input('m_wwbb:', min_value=0.001, max_value=10.0, value=1.0,key="2",format="%.4f")
+m_wwbb = st.number_input('m_wwbb: Particle mass at the W+-W+-bb final state', min_value=0.001, max_value=10.0, value=1.0,key="2",format="%.4f")
 
 jet_1_pt = st.number_input('jet_1_pt: The transverse momentum of the leading jet, that is the jet with largest transverse momentum', min_value=0.001, max_value=10.0, value=1.0,key="3",format="%.4f")
 
-m_wbb = st.number_input('m_wbb', min_value=0.001, max_value=10.0, value=1.0,key="4",format="%.4f")
+m_wbb = st.number_input('m_wbb: Particle mass at the W+-W+-b final state', min_value=0.001, max_value=10.0, value=1.0,key="4",format="%.4f")
 
-m_jjj = st.number_input('m_jjj', min_value=0.001, max_value=10.0, value=1.0,key="5",format="%.4f")
+m_jjj = st.number_input('m_jjj: The invariant mass of the three jets ', min_value=0.001, max_value=10.0, value=1.0,key="5",format="%.4f")
 
 
 if st.button('Predict '):
